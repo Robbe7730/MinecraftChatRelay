02c4b96e2a44a23a0805a8ccb41ada053e8302a0
Robbe Van Herck <robbe@robbevanherck.be>
2020-06-06 11:55:27 +0200

    Fix server not stopping due to hanging threads

--- src/main/java/be/robbevanherck/chatplugin/services/discord/DiscordChatService.java
+++ src/main/java/be/robbevanherck/chatplugin/services/discord/DiscordChatService.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.minecraft.server.MinecraftServer;
+import okhttp3.OkHttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,16 +15,16 @@ import javax.security.auth.login.LoginException;
 
 public class DiscordChatService extends ChatService {
     protected static final Logger LOGGER = LogManager.getLogger();
+    private JDA jda;
 
     @Override
     public void serverStarted(MinecraftServer server) {
         String token = (String) PropertiesRepository.getProperty("discord-token");
-        System.err.println(token);
         try {
             JDABuilder builder = new JDABuilder(token);
             builder.setActivity(Activity.playing("Minecraft: " + server.getServerMotd()));
 
-            JDA jda = builder.build();
+            jda = builder.build();
 
             jda.addEventListener(new DiscordMessageListener(this));
         } catch (LoginException e) {
@@ -34,12 +35,14 @@ public class DiscordChatService extends ChatService {
     @Override
     public void serverStopped(MinecraftServer server) {
         if (DiscordRepository.getChannel() != null) {
-            JDA jda = DiscordRepository.getChannel().getJDA();
-
             DiscordRepository.setChannel(null);
-
-            jda.shutdown();
         }
+        jda.shutdown();
+
+        // Force the HTTP/2 clients to close
+        OkHttpClient client = jda.getHttpClient();
+        client.connectionPool().evictAll();
+        client.dispatcher().executorService().shutdown();
     }
 
     @Override
